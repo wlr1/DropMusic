c370147434c3a53c3ee8862f2f384cf05a029566
wlr1 <uns4d123@gmail.com>
2023-07-29 18:39:16 +0300

    time slider logic

--- src/Components/MusicPlayer.tsx
+++ src/Components/MusicPlayer.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from 'react';
 import { FaPlay, FaPause, FaRedoAlt } from 'react-icons/fa';
 import { useLocation } from 'react-router-dom';
 
@@ -5,8 +6,63 @@ const MusicPlayer = () => {
   const location = useLocation();
   const selectedFile = location.state?.selectedFile || null;
 
+  // Set up state for audio playback
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+
+  //time slider
+  const [duration, setDuration] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.addEventListener('loadeddata', () => {
+        setDuration(audioRef.current?.duration || 0);
+      });
+
+      audioRef.current.addEventListener('timeupdate', () => {
+        setCurrentTime(audioRef.current?.currentTime || 0);
+      });
+    }
+  }, []);
+
+  //play audio
+  useEffect(() => {
+    if (selectedFile && audioRef.current) {
+      const fileURL = URL.createObjectURL(selectedFile);
+      audioRef.current.src = fileURL;
+      audioRef.current.load();
+    }
+  }, [selectedFile]);
+
+  const handlePlayPause = () => {
+    if (!audioRef.current) return;
+
+    if (!isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play().catch((error) => {
+        console.error('Error while playing music:', error);
+        setIsPlaying(false);
+      });
+    }
+    setIsPlaying(!isPlaying);
+  };
+
+  if (!selectedFile) {
+    return <div>No audio file selected.</div>;
+  }
+
   //Track title
   const trackTitle = selectedFile ? selectedFile.name : 'No track selected';
+
+  const formatTime = (time: number) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${minutes.toString().padStart(2, '0')}:${seconds
+      .toString()
+      .padStart(2, '0')}`;
+  };
 
   return (
     <div
@@ -33,7 +89,7 @@ const MusicPlayer = () => {
           <span
             className="text-white font-semibold text-2xl animate-marquee min-w-[700px]"
             style={{
-              animation: 'marquee 10s linear infinite', // Apply the animation
+              animation: 'marquee 15s linear infinite', // Apply the animation
             }}
           >
             {trackTitle}
@@ -41,24 +97,36 @@ const MusicPlayer = () => {
         </div>
         {/* Music Player Controls */}
         <div className="flex items-center justify-center space-x-6 mb-6">
-          <button className="p-2 bg-opacity-80 bg-white rounded-lg shadow-md hover:scale-110 transition-transform">
-            <FaRedoAlt className="text-white" />
+          <button
+            className="p-2 bg-opacity-80 bg-white rounded-lg shadow-md hover:scale-110 transition-transform"
+            onClick={handlePlayPause}
+          >
+            {isPlaying ? (
+              <FaPause className="text-white" />
+            ) : (
+              <FaPlay className="text-white text-3xl" />
+            )}
           </button>
           <button className="p-4 bg-opacity-80 bg-white rounded-full shadow-md hover:scale-110 transition-transform">
-            <FaPlay className="text-white text-3xl" />
-          </button>
-          <button className="p-2 bg-opacity-80 bg-white rounded-lg shadow-md hover:scale-110 transition-transform">
-            <FaPause className="text-white" />
+            <FaRedoAlt className="text-white" />
           </button>
         </div>
         {/* Time Slider */}
         <div className="flex items-center justify-center space-x-2 text-white mb-2">
-          <span>0:00</span>
+          <span>{formatTime(currentTime)}</span>
           <input
             type="range"
             className="w-64 h-2 appearance-none bg-white bg-opacity-50 rounded-lg outline-none cursor-pointer hover:bg-opacity-70 active:bg-opacity-80 transition-opacity"
+            min="0"
+            max={duration}
+            value={currentTime}
+            onChange={(e) => {
+              if (audioRef.current) {
+                audioRef.current.currentTime = parseInt(e.target.value, 10);
+              }
+            }}
           />
-          <span>3:45</span>
+          <span>{formatTime(duration)}</span>
         </div>
         {/* Sound Slider */}
         <div className="flex items-center justify-center space-x-2 text-white">
@@ -69,6 +137,8 @@ const MusicPlayer = () => {
           />
           <span>100%</span>
         </div>
+        {/* Audio Element */}
+        <audio ref={audioRef} />
       </div>
     </div>
   );
